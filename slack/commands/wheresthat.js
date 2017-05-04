8daccf5ad90df76bc0f60c65ea9baed78110ec84
Duncan Ross <duncan.ross@685b35ca6598.myob.myobcorp.net>
2017-05-04 13:43:40 +1000

    Demo Code Runing In Dev

--- slack/commands/wheresthat.js
+++ slack/commands/wheresthat.js
@@ -1,6 +1,6 @@
 var Trello = require("node-trello");
 var t = new Trello("6cabc9ff538609ad5eb9c9f75d20b60f", "f091df8c331042ef605d9db9dfb83bed2dba2e60a3de963ea44820eb01e34927");
-var files = ["johnny 5", "file32.js", "file44.java", "file4.html", "file15.js"];
+var request = require('request');
 
 var rooms = [
   {name: "mcp", location: "http://www.getcoloringpages.com/images/o9/o92vsjv.jpg"},
@@ -31,7 +31,7 @@ function getNamesAndTeams(callback) {
 
 function ResolveURI(data, text){
   var filtered = data.find(function(datum) {
-        return datum.personName.indexOf(text) != -1;
+        return datum.personName.toUpperCase().indexOf(text.toUpperCase()) != -1;
     }
   )
 
@@ -41,16 +41,32 @@ function ResolveURI(data, text){
 
 
 function WheresThatCommandHandler(token, command, text, reply, callback) {
+  
   function finish(data) {
     
     var result1 = data.find(function(datum){
-      return datum.name.indexOf(text) !== -1;
+        return datum.name.toUpperCase().indexOf(text.toUpperCase()) !== -1;
     });
 
-    //var teamName = result1.location.substring(result1.localtion.indexOf("("),result1.location.length);
-    //var mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + teamName + ".png";
+    var teamName = text;
+    var mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + text.toLowerCase().replace(" ","") + ".PNG";
+    var teamPreText = "Looks like " + text + " is the " + text + " Area:";
+    
+    if(typeof result1 !== 'undefined'){
+        teamName = result1.location.substring(0,result1.location.indexOf("("));
+        mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + teamName.toLowerCase().replace(" ","") + ".PNG";
+        teamPreText = "Looks like " + text + " is " + result1.name + " and is in " + teamName + " Team:";
+    }
 
-    reply(`Hey, <@${command.user}>,${result1.localtion.indexOf("(")} is this what you are looking for ?`, (err, result) => {
+
+    request(mapUri, function (error, response, body) {
+      if(response.statusCode === 404){
+        mapUri = "https://media-cdn.tripadvisor.com/media/photo-s/02/bd/e8/b0/the-cherry-tree-hotel.jpg";
+        teamPreText = "I couldnt find any matches for " + text + " you should shorten the size of the search?"
+      }
+    
+
+    reply(`Hey, <@${command.user}>, is this what you are looking for ?`, (err, result) => {
         
       return callback(null, {
       "token":token,
@@ -61,35 +77,19 @@ function WheresThatCommandHandler(token, command, text, reply, callback) {
           {
               "fallback": "Required plain-text summary of the attachment.",
               "color": "#36a64f",
-              //"pretext": `Where's That?`,
-              //"pretext": JSON.stringify(result1.location.substring(result1.location.indexOf(text),result1.location.length()),
-              // "author_name": "Bobby Tables",
+              "pretext": teamPreText,
+              "author_name": "MYOB.maps",
               // "author_link": "http://flickr.com/bobby/",
-              // "author_icon": "http://flickr.com/icons/bobby.jpg",
-              // "title": "Slack API Documentation",
-              // "title_link": "https://api.slack.com/",
-              // "text": "result",
-              // "fields": [
-              //     {
-              //         "title": "Priority",
-              //         "value": "High",
-              //         "short": false
-              //     }
-              // ],
-              "image_url": "https://media-cdn.tripadvisor.com/media/photo-s/02/bd/e8/b0/the-cherry-tree-hotel.jpg"
-              // "footer": "Slack API",
-              // "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-              // "ts": 123456789
+               "author_icon": "https://image.shutterstock.com/z/stock-vector-vector-map-icon-with-red-flag-108851135.jpg",
+               "title": "MYOB.maps",
+              "image_url": mapUri,
           }
       ]
     });
-
+});
     });
   }
-  getNamesAndTeams(finish);
-  // if (!rooms[text]) getNamesAndTeams(finish);
-  // else finish(rooms);
 
-   
+  getNamesAndTeams(finish); 
 };
 module.exports = WheresThatCommandHandler;
