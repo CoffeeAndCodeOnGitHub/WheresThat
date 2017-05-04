e4f9f6d77ac5abdd801de7e2d498d832351ef93f
Duncan Ross <duncan.ross@685b35ca6598.myob.myobcorp.net>
2017-05-04 14:38:35 +1000

    some hackday stuff

--- slack/commands/wheresthat.js
+++ slack/commands/wheresthat.js
@@ -49,29 +49,23 @@ function WheresThatCommandHandler(token, command, text, reply, callback) {
     });
 
     var teamName = text;
-    var mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + text.toLowerCase().replace(" ","") + ".PNG";
-    var teamPreText = "Looks like " + text + " is the " + text + " Area:";
+    var mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + text.toLowerCase().replace(" ","").replace("the","").replace("'","") + ".PNG";
+    var teamPreText = "Looks like you mean " + text + " that's highlighted in green on the map below!";
     
     if(typeof result1 !== 'undefined'){
         teamName = result1.location.substring(0,result1.location.indexOf("("));
         mapUri = "https://s3-ap-southeast-2.amazonaws.com/myob-map/rooms/" + teamName.toLowerCase().replace(" ","") + ".PNG";
-        teamPreText = "Looks like " + text + " is " + result1.name + " and is in " + teamName + " Team:";
+        teamPreText = "Looks like " + text + " is " + result1.name + " and is in " + teamName + " Team highlighted in green on the map below!";
     }
 
 
     request(mapUri, function (error, response, body) {
       if(response.statusCode === 404){
         mapUri = "https://media-cdn.tripadvisor.com/media/photo-s/02/bd/e8/b0/the-cherry-tree-hotel.jpg";
-        teamPreText = "I couldnt find any matches for " + text + " you should shorten the size of the search?"
+        teamPreText = "I couldnt find any matches for " + text + " type 'Rooms' or 'Teams' to see a list of available search options"
       }
     
-
-    reply(`Hey, <@${command.user}>, is this what you are looking for ?`, (err, result) => {
-        
-      return callback(null, {
-      "token":token,
-      "channel":token.channel,
-      "attachments": [
+      var attachment = [
           {
               "fallback": "Required plain-text summary of the attachment.",
               "color": "#36a64f",
@@ -80,8 +74,166 @@ function WheresThatCommandHandler(token, command, text, reply, callback) {
                "author_icon": "https://image.shutterstock.com/z/stock-vector-vector-map-icon-with-red-flag-108851135.jpg",
                "title": "MYOB.maps",
               "image_url": mapUri,
+
           }
       ]
+
+      if(text.toLowerCase() === 'teams'){
+        teamPreText = "I can see you need help with team names?"
+        attachment = [
+          {
+              "fallback": "Required plain-text summary of the attachment.",
+              "color": "#36a64f",
+              "pretext": teamPreText,
+              "fields": [
+                {
+                    "title": "Andromeda",
+                    "short": false
+                },
+                {
+                    "title": "Area 51",
+                    "short": false
+                },
+                {
+                    "title": "Betelgeuse",
+                    "short": false
+                },
+                {
+                    "title": "Don't Panic",
+                    "short": false
+                },
+                {
+                    "title": "Genesis",
+                    "short": false
+                },
+                {
+                    "title": "Juno",
+                    "short": false
+                },
+                {
+                    "title": "Kamino",
+                    "short": false
+                },
+                {
+                    "title": "Kristi",
+                    "short": false
+                },
+                {
+                    "title": "Odyssey",
+                    "short": false
+                },
+                {
+                    "title": "Orbital",
+                    "short": false
+                },
+                {
+                    "title": "Pathfinder",
+                    "short": false
+                },
+                {
+                    "title": "Replicant",
+                    "short": false
+                },
+                {
+                    "title": "Sora",
+                    "short": false
+                },
+                {
+                    "title": "Super Nova",
+                    "short": false
+                },
+                {
+                    "title": "Titan",
+                    "short": false
+                }
+                ]
+            }
+          ]
+      }
+
+      if(text.toLowerCase() === 'rooms'){
+        teamPreText = "I can see you need help with room names?"
+        attachment = [
+          {
+              "fallback": "Required plain-text summary of the attachment.",
+              "color": "#36a64f",
+              "pretext": teamPreText,
+              "fields": [
+                {
+                    "title": "Johnny 5",
+                    "short": false
+                },
+                {
+                    "title": "Holly",
+                    "short": false
+                },
+                {
+                    "title": "Rosie",
+                    "short": false
+                },
+                {
+                    "title": "R2-D2",
+                    "short": false
+                },
+                {
+                    "title": "MCP",
+                    "short": false
+                },
+                {
+                    "title": "Matrix",
+                    "short": false
+                },
+                {
+                    "title": "Marvin",
+                    "short": false
+                },
+                {
+                    "title": "M-B9",
+                    "short": false
+                },
+                {
+                    "title": "Kitt",
+                    "short": false
+                },
+                {
+                    "title": "Hal 9000",
+                    "short": false
+                },
+                {
+                    "title": "Wintermute",
+                    "short": false
+                },
+                {
+                    "title": "Skynet",
+                    "short": false
+                },
+                {
+                    "title": "ExMachina",
+                    "short": false
+                }
+              ]
+            }
+          ]
+      }
+
+    reply(`Hey, <@${command.user}>, is this what you are looking for ?`, (err, result) => {
+        
+      return callback(null, {
+      "token":token,
+      "channel":token.channel,
+      "attachments": attachment
+      //"attachments": [
+      //    {
+      //        "fallback": "Required plain-text summary of the attachment.",
+      //        "color": "#36a64f",
+      //        "pretext": teamPreText,
+      //        "author_name": "MYOB.maps",
+      //         "author_icon": "https://image.shutterstock.com/z/stock-vector-vector-map-icon-with-red-flag-108851135.jpg",
+      //         "title": "MYOB.maps",
+      //        "image_url": mapUri,
+
+//          }
+  //    ]
     });
 });
     });
